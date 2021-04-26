5a0454b6709948115938372820c768d7bfb3edee
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2021-04-26 13:29:33 -0300

    Fix eye position when turning

--- Assets/Scripts/Player/EyeController.cs
+++ Assets/Scripts/Player/EyeController.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class EyeController : MonoBehaviour
+public class EyeController : NetworkBehaviour
 {
     [SerializeField] Transform m_EyeTransform = default;
 
     Rigidbody2D m_Rigidbody;
 
+    [SyncVar]
     float facing = 1f;
 
     // Start is called before the first frame update
@@ -20,12 +22,15 @@ public class EyeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        m_EyeTransform.localScale = new Vector3(facing, 1f, 1f);
+
+        if (!isLocalPlayer)
+            return;
+
         if (m_Rigidbody.velocity.x < 0) {
             facing = -1f;
         } else if (m_Rigidbody.velocity.x > 0) {
             facing = 1f;
         }
-
-        m_EyeTransform.localScale = new Vector3(facing, 1f, 1f);
     }
 }
